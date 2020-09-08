9b090d201df942d33eeb7d4b4f210bfb75587bd8
Shahrulazriee <shahrulazrie.sa@gmial.com>
2020-09-08 15:56:50 +0800

    edit some js

--- admin/index/staffinformation.php
+++ admin/index/staffinformation.php
@@ -68,4 +68,19 @@
 		?> -->
 		</form>
 	</section>
-	<?php include "footer.php";?>
+	<script type="text/javascript" src="js/main.js">
+		/* staffinformation */
+		//for search filter
+		$(document).ready(function() {
+			$("#myInput").on("keyup", function() {
+				var value = $(this).val().toLowerCase();
+				$("#tablefortoday tr").filter(function() {
+					$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+				});
+			});
+		});
+		$('.popover-dismiss').popover({
+			trigger: 'focus'
+		})
+	</script>
+	<?php require "footer.php";?>
