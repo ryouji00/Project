6ad189b35d6d54d864630d055668907e5c542642
Shahrulazriee <shahrulazrie.sa@gmial.com>
2020-09-08 10:17:18 +0800

    remove wafiq part

--- sistemegerak/footer.php
+++
@@ -1,5 +0,0 @@
-<main>
-	<div class="footer" style="position: fixed;">
-		<p>Hak Cipta Terpelihara KKM@2020</p>
-	</div>
-</main>
--- sistemegerak/includes/dbh.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'localhost';
-$user = 'Azrie00';
-$pass = '';
-$db = 'egerak';
-
-$conn = mysqli_connect($host, $user, $pass, $db) or die("Could not connect the database");
