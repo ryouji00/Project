6293d02969c5993eae5964c9fe495e6eb6b658dc
Shahrulazriee <shahrulazrie.sa@gmial.com>
2020-09-11 09:39:03 +0800

    delete unrelated file

--- admin/include/dashboard.inc2.php
+++
@@ -1,5 +0,0 @@
-<tr id="">
-	<td><?php echo $row2["destinationname"];?></td>
-	<td><?php echo $row2["starttrip"];?></td>
-	<td><?php echo $row2["endtrip"];?></td>
-</tr>
--- admin/include/deletetrip.inc.php
+++
@@ -1,4 +0,0 @@
-<form method="get">
-    <button type="button" name="confirmdelete" class="btn btn-danger">Delete</button>
-    <a href="editinformation.php" class="btn btn-secondary">Cancel</a>
-</form>
