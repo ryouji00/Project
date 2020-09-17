6029df3fe67b7454d9331014b21127f25eb26450
Shahrulazriee <shahrulazrie.sa@gmial.com>
2020-09-17 15:52:43 +0800

    add margerine

--- sistemegerak/signup.php
+++ sistemegerak/signup.php
@@ -11,40 +11,39 @@
 
  	<div class="wrapper-main">
  		<section class="section-default">
- 	<h1>Signup</h1>
- 	<?php
-      if(isset($_GET["error"])){
-      	if($_GET["error"] == "emptyfields"){
-      		echo '<p class="signuperror">Fill in all fields!</p>';
-      	}
-      	elseif ($_GET["error"] == "invaliduidmail") {
-      		echo '<p class="signuperror">Invalid username and e-mail!</p>';
-      	}
-      	elseif ($_GET["error"] == "invaliduid") {
-      		echo '<p class="signuperror">Invalid username!</p>';
-      	}
-      	elseif ($_GET["error"] == "invalidmail") {
-      		echo '<p class="signuperror">Invalid e-mail!</p>';
-        }
-        elseif ($_GET["error"] == "passwordcheck") {
-        	echo '<p class="signuperror">Your password do not match!</p>';
-        }
-        elseif ($_GET["error"] == "usertaken") {
-        	echo '<p class="signuperror">Username is already taken!</p>';
-        }
-    } 
-      elseif ($_GET["signup"] == "success") {
-      	echo '<p class="signupSuccess">Signup successful!</p>';
-      }
-
- 	?>
- 	<form action="includes/signup.inc.php" method="post"> 
-    <input type="text" name="nama" placeholder="Name">
-    <select name="position">
-   		<option value="Admin">Admin</option>
-   		<option value="Staff">Staff</option>
-    </select>
-    <select name="unit">
+		<h1>Signup</h1>
+		<?php
+		if(isset($_GET["error"])){
+			if($_GET["error"] == "emptyfields"){
+				echo '<p class="signuperror">Fill in all fields!</p>';
+			}
+			elseif ($_GET["error"] == "invaliduidmail") {
+				echo '<p class="signuperror">Invalid username and e-mail!</p>';
+			}
+			elseif ($_GET["error"] == "invaliduid") {
+				echo '<p class="signuperror">Invalid username!</p>';
+			}
+			elseif ($_GET["error"] == "invalidmail") {
+				echo '<p class="signuperror">Invalid e-mail!</p>';
+			}
+			elseif ($_GET["error"] == "passwordcheck") {
+				echo '<p class="signuperror">Your password do not match!</p>';
+			}
+			elseif ($_GET["error"] == "usertaken") {
+				echo '<p class="signuperror">Username is already taken!</p>';
+			}
+		}
+		elseif ($_GET["signup"] == "success") {
+			echo '<p class="signupSuccess">Signup successful!</p>';
+		}
+		?>
+ 		<form action="includes/signup.inc.php" method="post"> 
+    		<input type="text" name="nama" placeholder="Name">
+    		<select name="position">
+   				<option value="Admin">Admin</option>
+   				<option value="Staff">Staff</option>
+    		</select>
+    		<select name="unit">
 				<option value="Unit Rangkaian">Unit Rangkaian</option>
 				<option value="Unit Sokongan Teknikal">Unit Sokongan Teknikal</option>
 				<option value="Unit Operasi dan Pusat">Unit Operasi dan Pusat</option>
@@ -59,25 +58,15 @@
 				<option value="Unit Perolehan SQA">Unit Perolehan SQA</option>
 				<option value="Unit Pengurusan, Pentadbiran, Kewangan">Unit Pengurusan, Pentadbiran, Kewangan</option>
 				<option value="Unit Perancangan dan Inovasi ICT">Unit Perancangan dan Inovasi ICT</option>
-	</select>
- 	<input type="text" name="uid" placeholder="Username">
- 	<input type="text" name="mail" placeholder="E-mail">
- 	<input type="password" name="pwd" placeholder="Password">
- 	<input type="password" name="pwd-repeat" placeholder="Repeat password">
-  <button type="submit" name="signup-submit">Signup</button>
-
-
-  <a class="btn btn-outline-secondary" href="login.php">Login</a>
-
-
- </form>
-
-</section>
+			</select>
+ 			<input type="text" name="uid" placeholder="Username">
+ 			<input type="text" name="mail" placeholder="E-mail">
+ 			<input type="password" name="pwd" placeholder="Password">
+ 			<input type="password" name="pwd-repeat" placeholder="Repeat password">
+			<button type="submit" name="signup-submit">Signup</button>
+			<a id="loginbtn" class="btn btn-outline-secondary" href="login.php">Login</a>
+		</form>
+	</section>
 </div>
- </main>
-  
-
-
- <?php
- require "footer.php";
- ?>
+</main>
+<?php require "footer.php";?>
