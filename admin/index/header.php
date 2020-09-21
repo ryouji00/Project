51fb425e5c25845b5fb0f8998cf101c431278ea9
Shahrulazriee <shahrulazrie.sa@gmial.com>
2020-09-21 08:08:25 +0800

    include jqueyr

--- admin/index/header.php
+++ admin/index/header.php
@@ -63,3 +63,5 @@ include_once 'configdb.php';
 		<!-- Search Filter Bootstrap -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
