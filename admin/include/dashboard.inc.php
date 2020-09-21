141e8da1ab92b75f32ee4ba9ba3bc4cd8544ee13
Shahrul Mahizan Azrie Bin Mazlan <50312396+Shahrulazriee@users.noreply.github.com>
2020-09-21 16:20:09 +0800

    delete

--- admin/include/dashboard.inc.php
+++ admin/include/dashboard.inc.php
@@ -29,13 +29,13 @@
 				$result2 = $conn -> query($sql2);
 				if(mysqli_num_rows($result2) > 0) {
 					while($row2 = mysqli_fetch_assoc($result2)) {
-							include "dateformat.inc.php";
-							echo "Kategori: " .$row2["category"];
-							echo "<br>Tugasan: " .$row2["workname"];
-							echo "<br>Lokasi: " .$row2["placename"];
-							echo "<br>Pegawai pengganti: " .$row2["replaceofficer"];
-							echo "<br>Dari: " .$newDate. " hingga " .$newDate2;
-							echo "<br>Bermula: " .$newTime . " hingga " .$newTime2;
+						include "dateformat.inc.php";
+						echo "Kategori: " .$row2["category"];
+						echo "<br>Tugasan: " .$row2["workname"];
+						echo "<br>Lokasi: " .$row2["placename"];
+						echo "<br>Pegawai pengganti: " .$row2["replaceofficer"];
+						echo "<br>Dari: " .$newDate. " hingga " .$newDate2;
+						echo "<br>Bermula: " .$newTime . " hingga " .$newTime2;
 					}
 				}
 				else {
