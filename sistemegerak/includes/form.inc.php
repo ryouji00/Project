e42f894c33f276c74af9b687b2c59783957a1b79
ryouji00 <mryouj00@gmail.com>
2020-09-23 10:56:09 +0800

    new line

--- sistemegerak/includes/form.inc.php
+++ sistemegerak/includes/form.inc.php
@@ -34,7 +34,7 @@ if(isset($_POST['send-submit'])){
 				exit();
 			}
 			else{
-				$sql = "INSERT INTO destination ('category','workname','placename','tripstart','tripend','timestart','timeend','replaceofficer','staffid') VALUE (?,?,?,?,?,?,?,?,?)";
+				$sql = "INSERT INTO destination ('category','workname','placename','tripstart','tripend','timestart','timeend','replaceofficer') VALUE (?,?,?,?,?,?,?,?)";
 				$stmt = mysqli_stmt_init($conn);
 				if(!mysqli_stmt_prepare($stmt,$sql)){
 					header("Location: ../form.php?error=sqlerror2");
