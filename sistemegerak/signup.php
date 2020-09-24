e4de4cb5c39f37b8e20fdcccffd944a2a5e0d54b
Shahrulazriee <shahrulazrie.sa@gmial.com>
2020-09-24 14:21:29 +0800

    line 16

--- sistemegerak/signup.php
+++ sistemegerak/signup.php
@@ -13,29 +13,32 @@
  		<section class="section-default">
 		<h1>Signup</h1>
 		<?php
-		if(isset($_GET["error"])){
-			if($_GET["error"] == "emptyfields"){
-				echo '<p class="signuperror">Fill in all fields!</p>';
+		if(isset($_GET['signup'])) {
+			if(isset($_GET["error"])){
+				if($_GET["error"] == "emptyfields"){
+					echo '<p class="signuperror">Fill in all fields!</p>';
+				}
+				elseif ($_GET["error"] == "invaliduidmail") {
+					echo '<p class="signuperror">Invalid username and e-mail!</p>';
+				}
+				elseif ($_GET["error"] == "invaliduid") {
+					echo '<p class="signuperror">Invalid username!</p>';
+				}
+				elseif ($_GET["error"] == "invalidmail") {
+					echo '<p class="signuperror">Invalid e-mail!</p>';
+				}
+				elseif ($_GET["error"] == "passwordcheck") {
+					echo '<p class="signuperror">Your password do not match!</p>';
+				}
+				elseif ($_GET["error"] == "usertaken") {
+					echo '<p class="signuperror">Username is already taken!</p>';
+				}
 			}
-			elseif ($_GET["error"] == "invaliduidmail") {
-				echo '<p class="signuperror">Invalid username and e-mail!</p>';
-			}
-			elseif ($_GET["error"] == "invaliduid") {
-				echo '<p class="signuperror">Invalid username!</p>';
-			}
-			elseif ($_GET["error"] == "invalidmail") {
-				echo '<p class="signuperror">Invalid e-mail!</p>';
-			}
-			elseif ($_GET["error"] == "passwordcheck") {
-				echo '<p class="signuperror">Your password do not match!</p>';
-			}
-			elseif ($_GET["error"] == "usertaken") {
-				echo '<p class="signuperror">Username is already taken!</p>';
+			elseif ($_GET["signup"] == "success") {
+				echo '<p class="signupSuccess">Signup successful!</p>';
 			}
 		}
-		elseif ($_GET["signup"] == "success") {
-			echo '<p class="signupSuccess">Signup successful!</p>';
-		}
+		
 		?>
  		<form action="includes/signup.inc.php" method="post"> 
     		<input type="text" name="nama" placeholder="Name">
