4fcda3b362cc5eec5e725760b0a58a1140ede2f5
Shahrulazriee <shahrulazrie.sa@gmial.com>
2020-09-25 09:11:09 +0800

    ffasd

--- sistemegerak/header.php
+++ sistemegerak/header.php
@@ -21,8 +21,33 @@ include_once 'includes/dbh.inc.php';
 		<img class="firstimg" src="img\jatanegara2.png" width="160">
 		<img class="secondimg" src="img\kkm2.png" width="160">
 		<span><h1 class="tajuk">Sistem E-Pergerakan Pegawai</h1></span>
-
+</div>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light toggled">
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav nav-tabs mr-auto">
+							<a class="btn btn-lg navbar-brand" href="http://localhost/Project/sistemegerak/form.php?form=success">
+								<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+									<path fill-rule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z"/>
+									<path fill-rule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+								</svg>
+							</a>
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon"></span>
+							</button>
+    						<li class="nav-item active">
+    							<a class="nav-link" href="http://localhost/Project/sistemegerak/history.php">Sejarah Perjalanan<span class="sr-only">(current)</span></a>
+    						</li>
+							<li class="nav-item">
+								<a id="logoutbtn" class="btn btn-outline-secondary" href="http://localhost/Project/admin/include/logout.inc.php">Daftar keluar
+									<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-lock" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+										<path fill-rule="evenodd" d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"/>
+									</svg>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
 		
-	</div>
+
 </body>
 </html>
---
+++ sistemegerak/history.php
@@ -0,0 +1,28 @@
+<?php require 'header.php';?>
+
+<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/whole.css">
+<body>
+    <section class="container">
+        <?php
+        $id = $_SESSION['idstaff'];
+        $sql ="SELECT *
+                FROM destination
+                WHERE staffid = $id";
+        $result = $conn -> query($sql);
+        if($result -> num_rows > 0) {
+            while($row2 = $result -> fetch_assoc()) {
+                include '../admin/include/dateformat.inc.php';
+                echo "<br><b>Trip ID: " .$row2['destinationid']. "</b>";
+                echo "<br>Category: " .$row2['category'];
+                echo "<br>Workname: " .$row2['workname'];
+                echo "<br>Place: " .$row2['placename'];
+                echo "<br>Date: " .$newDate. " hingga " .$newDate2;
+                echo "<br>Date: " .$newTime. " hingga " .$newTime2. "<hr>";
+            }
+        }
+        ?>
+    </section>
+    
+</body>
