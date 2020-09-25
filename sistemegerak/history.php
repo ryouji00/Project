9b9d7aa5adafd68fe39966ab6b22362987540cab
Shahrulazriee <shahrulazrie.sa@gmial.com>
2020-09-25 09:12:39 +0800

    FFASD

--- sistemegerak/history.php
+++ sistemegerak/history.php
@@ -9,7 +9,8 @@
         $id = $_SESSION['idstaff'];
         $sql ="SELECT *
                 FROM destination
-                WHERE staffid = $id";
+                WHERE staffid = $id
+                ORDER BY tripstart ASC";
         $result = $conn -> query($sql);
         if($result -> num_rows > 0) {
             while($row2 = $result -> fetch_assoc()) {
